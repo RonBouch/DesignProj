694bce95cf1d18a65abe78b87173f9379902b4fd
hai eliasi <eliasihai@hotmail.com>
2019-07-14 16:47:09 +0300

    facebook update

--- NightLife/App.js
+++ NightLife/App.js
@@ -8,6 +8,7 @@ import HomeMenuView from "./Components/HomeMenuView.js";
 import PartyPage from "./Components/PartyPage.js";
 import Publish from "./Components/Publish";
 import Camera from "./Components/Camera.js";
+import FaceBookPage from './Components/FaceBookPage';
 
 class App extends React.Component {
   render() {
@@ -22,6 +23,7 @@ const StackNav = createStackNavigator(
     PartyPage: PartyPage,
     Login: Login,
     Camera: Camera,
+    FaceBookPage: FaceBookPage,
 
     HomeMenuView: HomeMenuView,
     Register: Register
